c16f17bfbf4669047640780259a46d27d45c6cec
yovizn <yovizulkarnaen@gmial.com>
2024-04-05 13:42:38 +0700

    perf(globals.css): set-up base styling

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,11 @@
-import Image from "next/image";
+import Image from 'next/image'
 
 export default function Home() {
-  return (
-    <main className="min-h-screen w-full">
-      <div className="h-screen w-full flex items-center justify-center">
-        Instagram
-      </div>
-    </main>
-  );
+    return (
+        <main className='min-h-screen w-full'>
+            <div className='h-screen w-full flex items-center justify-center'>
+                <h1>noval</h1>
+            </div>
+        </main>
+    )
 }
